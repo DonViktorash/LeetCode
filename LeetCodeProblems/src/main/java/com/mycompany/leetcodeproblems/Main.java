8676fff241439ae36f747833edc143129500492f
DonViktorash <m.f-society@yandex.ru>
2020-04-04 03:24:58 +0300

    make file for problem solved

--- LeetCodeProblems/src/main/java/com/mycompany/leetcodeproblems/Main.java
+++ LeetCodeProblems/src/main/java/com/mycompany/leetcodeproblems/Main.java
@@ -12,5 +12,33 @@ package com.mycompany.leetcodeproblems;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int[] arr = {7,7,7,7,7,7,7};
+        int suspect = 0;
+        int counter = 0;
+        int max = -1;
+        int answer = -1;
+        for(int i=0; i<arr.length; i++){
+            System.out.println("Suspect is:"+arr[i]);
+            suspect = arr[i];
+            for(int j = 0; j < arr.length; j++){
+                if(suspect == arr[j]) {
+                    System.out.println("Suspect equal arr[i]");
+                    counter++;
+                }
+            }
+            //System.out.println(counter+" "+suspect);
+            if (counter == suspect) {
+                if (counter >= max) {
+                    System.out.println("counter" + counter + ">= max " + max);
+                    if (suspect > answer) {
+                        System.out.println("suspect >= answer");
+                        answer = suspect;
+                        max = counter;
+                    }
+                }
+            }
+            counter = 0;
+        }
+        System.out.println(answer);
     }
 }
